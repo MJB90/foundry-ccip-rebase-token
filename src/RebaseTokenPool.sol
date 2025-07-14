0413fb165c28215083a9ea8724d21a59b85eadbd
mjb90 <manasborah90@gmail.com>
2025-07-15 07:43:09 +0800

    Implemented lock or burn

--- src/RebaseTokenPool.sol
+++ src/RebaseTokenPool.sol
@@ -3,12 +3,33 @@
 pragma solidity ^0.8.24;
 
 import {TokenPool} from "@ccip/src/v0.8/ccip/pools/TokenPool.sol";
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IERC20} from "@ccip/src/v0.8/vendor/openzeppelin-solidity/v4.8.3/contracts/token/ERC20/IERC20.sol";
 import {Pool} from "@ccip/src/v0.8/ccip/libraries/Pool.sol";
+import {IRebaseToken} from "./interfaces/IRebaseToken.sol";
 
-// contract RebaseTokenPool is TokenPool {
+contract RebaseTokenPool is TokenPool {
+    constructor(IERC20 token, address[] memory _allowList, address _rnmProxy, address _router)
+        TokenPool(token, _allowList, _rnmProxy, _router)
+    {
+        // Constructor logic can be added here if needed
+    }
 
-//     constructor(IERC20 token, address[] memory _allowList, address _rnmProxy, address _router) TokenPool(token, _allowList, _rnmProxy, _router) {
-//         // Constructor logic can be added here if needed
-//     }
-// }
+    function lockOrBurn(Pool.LockOrBurnInV1 calldata lockOrBurnIn)
+        external
+        returns (Pool.LockOrBurnOutV1 memory lockOrBurnOut)
+    {
+        _validateLockOrBurn(lockOrBurnIn);
+        address receiver = abi.decode(lockOrBurnIn.receiver, (address));
+        uint256 userInterestRate = IRebaseToken(address(i_token)).getUserInterestRate(receiver);
+        IRebaseToken(address(i_token)).burn(address(this), lockOrBurnIn.amount);
+        lockOrBurnOut = Pool.LockOrBurnOutV1({
+            destTokenAddress: getRemoteToken(lockOrBurnIn.remoteChainSelector),
+            destPoolData: abi.encode(userInterestRate)
+        });
+    }
+
+    function releaseOrMint(Pool.ReleaseOrMintInV1 calldata releaseOrMintIn)
+        external
+        returns (Pool.ReleaseOrMintOutV1 memory)
+    {}
+}
